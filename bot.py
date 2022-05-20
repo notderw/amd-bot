d8f599453cdc15f0eebb0ae994f73a9be20e538b
Unexceptional <git@unexceptional.net>
2022-05-20 12:53:22 -0600

    run black

--- bot.py
+++ bot.py
@@ -16,11 +16,11 @@ from pydantic import BaseModel
 from derw import makeLogger
 
 # if we don't set this, praw utc timestamps take a shit
-os.environ['TZ'] = 'UTC'
+os.environ["TZ"] = "UTC"
 time.tzset()
 
 
-SUBREDDIT = os.environ.get('SUBREDDIT')
+SUBREDDIT = os.environ.get("SUBREDDIT")
 
 
 log = makeLogger(__name__)
@@ -46,12 +46,14 @@ class Config(BaseModel):
 
 class AMDBot:
     def __init__(self) -> None:
-        self.reddit = asyncpraw.Reddit('AMD', user_agent='/r/AMD bot by /u/RenegadeAI') # scopes: identity, modcontributors (for subreddit.mod.removal_reasons?!?!?), modposts, privatemessages, read, submit, wikiread
+        self.reddit = asyncpraw.Reddit(
+            "AMD", user_agent="/r/AMD bot by /u/RenegadeAI"
+        )  # scopes: identity, modcontributors (for subreddit.mod.removal_reasons?!?!?), modposts, privatemessages, read, submit, wikiread
 
     async def _load_config(self) -> None:
-        page = await self.subreddit.wiki.get_page('amd-bot')
+        page = await self.subreddit.wiki.get_page("amd-bot")
         self.config = Config(**yaml.safe_load(page.content_md))
-        log.debug(f'Loaded config for {self.subreddit.display_name}')
+        log.debug(f"Loaded config for {self.subreddit.display_name}")
 
     async def comment_and_remove(self, submission, reason_id) -> None:
         removal_reason = await self.subreddit.mod.removal_reasons.get_reason(reason_id)
@@ -64,7 +66,7 @@ class AMDBot:
         await submission.mod.lock()
 
     async def is_ts(self, submission) -> bool:
-        if not hasattr(submission, 'link_flair_template_id'):
+        if not hasattr(submission, "link_flair_template_id"):
             return False
 
         if submission.link_flair_template_id != self.config.tech_support_flair:
@@ -72,7 +74,6 @@ class AMDBot:
 
         if submission.approved_by:
             return False
-
 
         await self.subreddit.load()
 
@@ -82,7 +83,7 @@ class AMDBot:
 
         await self.comment_and_remove(submission, self.config.tech_support_rr)
 
-        log.info(f'{submission.shortlink} - Removed tech support')
+        log.info(f"{submission.shortlink} - Removed tech support")
 
         return True
 
@@ -90,38 +91,68 @@ class AMDBot:
         try:
             await submission.load()
 
-            log.debug(f'{submission.shortlink} - [{submission.link_flair_text}] {submission.title}')
+            log.debug(
+                f"{submission.shortlink} - [{submission.link_flair_text}] {submission.title}"
+            )
 
             if await self.is_ts(submission):
                 return
 
-            if hasattr(submission, 'link_flair_template_id') and \
-                    submission.link_flair_template_id == self.config.rumor_flair:
+            if (
+                hasattr(submission, "link_flair_template_id")
+                and submission.link_flair_template_id == self.config.rumor_flair
+            ):
                 comment = await submission.reply(self.config.rumor_message)
                 await comment.mod.distinguish(sticky=True)
                 return
 
             if not submission.link_flair_text:
-                log.info(f'{submission.shortlink} - Does not have flair')
+                log.info(f"{submission.shortlink} - Does not have flair")
 
-                sleep_time_until_message = self.config.time_until_message - (datetime.utcnow() - datetime.fromtimestamp(submission.created_utc)).total_seconds()
+                sleep_time_until_message = (
+                    self.config.time_until_message
+                    - (
+                        datetime.utcnow()
+                        - datetime.fromtimestamp(submission.created_utc)
+                    ).total_seconds()
+                )
                 if sleep_time_until_message > 0:
                     await asyncio.sleep(sleep_time_until_message)
 
-                add_flair_message = self.config.add_flair_message.format(post_url=submission.shortlink, time_until_remove=timedelta(seconds=self.config.time_until_remove)).strip()
+                add_flair_message = self.config.add_flair_message.format(
+                    post_url=submission.shortlink,
+                    time_until_remove=timedelta(seconds=self.config.time_until_remove),
+                ).strip()
 
-                sent_messages = [m async for m in self.reddit.inbox.sent() if m.body == add_flair_message]
+                sent_messages = [
+                    m
+                    async for m in self.reddit.inbox.sent()
+                    if m.body == add_flair_message
+                ]
                 if sent_messages:
-                    log.debug(f'{submission.shortlink} - Already sent message')
+                    log.debug(f"{submission.shortlink} - Already sent message")
 
-                    sleep_time_until_remove = self.config.time_until_remove - (datetime.utcnow() - datetime.fromtimestamp(sent_messages[0].created_utc)).total_seconds()
+                    sleep_time_until_remove = (
+                        self.config.time_until_remove
+                        - (
+                            datetime.utcnow()
+                            - datetime.fromtimestamp(sent_messages[0].created_utc)
+                        ).total_seconds()
+                    )
 
                 else:
-                    await submission.author.message(self.config.add_flair_subject, add_flair_message)
-                    log.debug(f'{submission.shortlink} - Sent message')
+                    await submission.author.message(
+                        self.config.add_flair_subject, add_flair_message
+                    )
+                    log.debug(f"{submission.shortlink} - Sent message")
 
-                    sleep_time_until_remove = self.config.time_until_remove - (datetime.utcnow() - datetime.fromtimestamp(submission.created_utc)).total_seconds()
-
+                    sleep_time_until_remove = (
+                        self.config.time_until_remove
+                        - (
+                            datetime.utcnow()
+                            - datetime.fromtimestamp(submission.created_utc)
+                        ).total_seconds()
+                    )
 
                 if sleep_time_until_remove > 0:
                     await asyncio.sleep(sleep_time_until_remove)
@@ -135,24 +166,28 @@ class AMDBot:
                     await submission.mod.remove()
                     return
 
-
             if submission.link_flair_template_id in self.config.battlestation_flairs:
                 # if posted day is not sat / sun
-                if not 5 <= datetime.fromtimestamp(submission.created_utc).weekday() <= 6:
-                    await self.comment_and_remove(submission, self.config.battlestation_rr)
-                    log.info(f'{submission.shortlink} - Removed battlestation')
+                if (
+                    not 5
+                    <= datetime.fromtimestamp(submission.created_utc).weekday()
+                    <= 6
+                ):
+                    await self.comment_and_remove(
+                        submission, self.config.battlestation_rr
+                    )
+                    log.info(f"{submission.shortlink} - Removed battlestation")
 
         except Exception as e:
             log.critical(traceback.format_exc())
             log.critical(e)
 
-
     async def run(self) -> None:
         self.me = me = await self.reddit.user.me()
-        log.info(f'Logged in as {me.name} - {me.id}')
+        log.info(f"Logged in as {me.name} - {me.id}")
 
         self.subreddit = subreddit = await self.reddit.subreddit(SUBREDDIT, fetch=True)
-        log.info(f'Watching /r/{subreddit.display_name}')
+        log.info(f"Watching /r/{subreddit.display_name}")
 
         await self._load_config()
 
